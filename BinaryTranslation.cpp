db5f8cc8c7f070607bb0ff375a4acf661ea512f8
rchen5799 <41244205+rchen5799@users.noreply.github.com>
2018-12-29 18:31:41 -0500

    Update BinaryTranslation.cpp

--- BinaryTranslation.cpp
+++ BinaryTranslation.cpp
@@ -1,12 +1,12 @@
 //============================================================================
-// Name        : HelloWorld.cpp
+// Name        : BinaryConvert.cpp
 // Author      : Rich
 // Version     : 1.0
-// Copyright   : Your copyright notice
 // Description : Binary Conversion (Decimal to Binary) Tool in C++, Ansi-style
 //============================================================================
 
 #include<iostream>
+#include <exception>
 #include<string>
 using namespace std;
 
@@ -30,10 +30,17 @@ int deciToBinary(int N) {
 }
 
 int main() {
+  int convert;
   cout<<"What number would you like to convert to binary? "<<endl;
   string x;
   cin>>x;
-  int convert = stoi(x);
+  try{
+    convert = stoi(x);
+  } catch(exception& e) {
+    cout<<"Unable to Convert " << x << " into Binary\n" <<endl;
+    return 1;
+  }
+
   cout<<deciToBinary(convert)<<" is the binary form"<<endl;
   return 0;
 }
