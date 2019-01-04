f45354252738228b0f07f814cc9c1da51ba2dd6e
rchen5799 <41244205+rchen5799@users.noreply.github.com>
2019-01-04 17:55:14 -0500

    Added Binary to Decimal and Better Main

--- BinaryTranslation.cpp
+++ BinaryTranslation.cpp
@@ -1,19 +1,29 @@
 //============================================================================
 // Name        : BinaryConvert.cpp
 // Author      : Rich
-// Version     : 1.0
-// Description : Binary Conversion (Decimal to Binary) Tool in C++, Ansi-style
+// Version     : 2.0
+// Description : Unsigned Binary Conversion (Decimal to Binary) Tool in C++, Ansi-style
 //============================================================================
 
 #include<iostream>
-#include <exception>
+#include<exception>
 #include<string>
+#include<math.h>
 using namespace std;
 
+//Converting Number into Unsigned Binary
 int deciToBinary(int N) {
   string binaryNum;
   int curr = N;
+  bool neg = false;
 
+  //for the case if the number entered is negative
+  if(N < 0){
+    curr = -curr;
+    neg = true;
+  }
+
+  //Converting the number into a string of binary digits
   while(curr!=0) {
       int remainder = curr%2;
       if(remainder == 1) {
@@ -26,21 +36,87 @@ int deciToBinary(int N) {
           binaryNum.insert(0,"0");
       }
   }
+
+  //Adds a negative if the number is negative
+  if(neg == true) {
+    binaryNum.insert(0,"-");
+  }
+  //Converts it back into an integer
   return stoi(binaryNum);
 }
 
-int main() {
-  int convert;
-  cout<<"What number would you like to convert to binary? "<<endl;
-  string x;
-  cin>>x;
-  try{
-    convert = stoi(x);
-  } catch(exception& e) {
-    cout<<"Unable to Convert " << x << " into Binary\n" <<endl;
-    return 1;
+//Converts Binary into a Decimal
+int binaryToDeci(int N) {
+
+  //Checks if input is negative
+  bool neg = false;
+  if(N<0) {
+    N = -N;
+    neg = true;
   }
 
-  cout<<deciToBinary(convert)<<" is the binary form"<<endl;
-  return 0;
+  string binary = to_string(N);
+  int power = binary.length()-1;
+  int count = 0;
+
+  //Keeps track of count (the final number)
+  for(int i = 0; i <= binary.length(); i++) {
+    if(binary[i] == '1') {
+      int raise = power - i;
+      count += pow(2, raise);
+    }
+  }
+
+  if(neg == true) {
+    count = -count;
+  }
+
+  return count;
 }
+
+
+int main() {
+  while(1) {
+    int convert;
+    string input;
+    do {
+      cout<<"What would you like to do? "<<endl;
+      cout<<"Enter 1 to convert from decimal to binary"<<endl;
+      cout<<"Enter 2 to convert from binary to decimal"<<endl;
+      cout<<"Enter 3 to exit"<<endl;
+      cout<<">>";
+      cin>>input;
+
+      if(input != "1" && input != "2" && input != "3") {
+        cout<<"Please enter a valid input!"<<endl;
+      }
+    } while(input != "1" && input != "2" && input != "3");
+
+    if(input == "1") {
+      cout<<"What number would you like to convert to binary? "<<endl;
+      string x;
+      cin>>x;
+      try{
+        convert = stoi(x);
+      } catch(exception& e) {
+        cout<<"Unable to Convert '" << x << "' into Binary \n" <<endl;
+        continue;
+      }
+      cout<<deciToBinary(convert)<<" is the unsigned binary form \n"<<endl;
+    } else if(input == "2") {
+      cout<<"What binary would you like to convert to decimal? "<<endl;
+      string x;
+      cin>>x;
+      try{
+        convert = stoi(x);
+      } catch(exception& e){
+        cout<< "Unable to Convert '" << x << "' into Decimal \n" <<endl;
+        continue;
+      }
+      cout<<binaryToDeci(convert)<<" is the decimal form \n"<<endl;
+    } else {
+      return 0;
+    }
+  }
+}
+
